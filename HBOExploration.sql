f384be3937e55b4dbd9310b22230c6f279a43f8b
Hienng08 <109943606+Hienng08@users.noreply.github.com>
2023-02-11 18:43:15 +0200

    Add files via upload

---
+++ HBOExploration.sql
@@ -0,0 +1,68 @@
+--Clean the columns imdb score ann tmdb_score 
+SELECT (CAST(imdb_score AS decimal)/10) AS new_imdb_score , (CAST(tmdb_score AS decimal)/10) AS new_tmdb_score
+FROM Hbo..HBOtiltes;
+
+--Sort for the highest imdb_score movie and show
+SELECT title, type, genres, (CAST(imdb_score AS decimal)/10) AS new_imdb_score, imdb_votes 
+FROM Hbo..HBOtiltes
+WHERE imdb_score  is not null 
+ORDER BY imdb_score DESC;
+
+
+--Sort for the highest tmdb_score movie and show
+SELECT title, type, genres, (CAST(tmdb_score AS decimal)/10) AS new_tmdb_score
+FROM Hbo..HBOtiltes
+WHERE tmdb_score is not null 
+ORDER BY tmdb_score DESC;
+
+
+-- Sort for the movies and shows with the largest imdb votes 
+SELECT title, type, genres, (CAST(imdb_score AS decimal)/10) AS new_imdb_score, imdb_votes
+FROM Hbo..HBOtiltes
+WHERE imdb_score  is not null AND imdb_votes is not NULL 
+ORDER BY  imdb_votes DESC; 
+
+--Find the show/movies with the highest total score = new_imdb_score * imdb_votes 
+SELECT title, type, genres, (CAST(imdb_score AS decimal)/10) AS new_imdb_score, imdb_votes, imdb_votes*(CAST(imdb_score AS decimal)/10) AS total_score
+FROM Hbo..HBOtiltes
+WHERE imdb_score  is not null AND imdb_votes is not NULL 
+ORDER BY   total_score DESC; 
+-- The movie The Shawshank Redemption has the highest score 
+
+--Find the shows with the highest total_score 
+SELECT title, type, genres, (CAST(imdb_score AS decimal)/10) AS new_imdb_score, imdb_votes, imdb_votes*(CAST(imdb_score AS decimal)/10) AS total_score
+FROM Hbo..HBOtiltes
+WHERE imdb_score  is not null 
+AND imdb_votes is not NULL 
+AND type LIKE '%SHOW%'
+ORDER BY   total_score DESC; 
+
+--Find the top 5 genres that are most common in the dataset using CTE 
+WITH genre_counts AS (
+  SELECT genres, COUNT(genres) AS count
+  FROM Hbo..HBOtiltes
+  WHERE genres IS NOT NULL
+  GROUP BY genres
+)
+SELECT TOP 5 genres, count
+FROM genre_counts
+ORDER BY count DESC;
+
+--Joins hbo credits and hbo titles table based on the id 
+SELECT *
+FROM Hbo..HBOtiltes tle 
+JOIN Hbo..HBOcredits cre
+ON tle.id = cre.id;  
+
+--Find the actors that appear most frequently in HBO movies and shows 
+WITH actor_counts AS (
+    SELECT name,role, COUNT(name) AS FrequentActor 
+    FROM Hbo..HBOtiltes tle 
+    JOIN Hbo..HBOcredits cre
+    ON tle.id = cre.id 
+    WHERE role = 'ACTOR'
+    GROUP BY name, role
+)
+SELECT TOP 5 name,role, FrequentActor
+FROM actor_counts
+ORDER BY FrequentActor DESC;
